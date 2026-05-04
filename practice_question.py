1b9bab2b018bac825df54b5418604dee0932c64e
SALONI SINGH <ssaloni3258@gmail.com>
2026-05-04 23:31:25 +0530

    Update practice_question.py

--- practice_question.py
+++ practice_question.py
@@ -1,1 +1,8 @@
+#  — Recursion (Reverse a Number)
+def reverse_number(n, rev=0):
+    if n == 0:
+        return rev
+    return reverse_number(n // 10, rev * 10 + n % 10)
 
+print(reverse_number(1234))
+print(reverse_number(120))
