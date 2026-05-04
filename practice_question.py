f9f91fc3e65460c5f831bfa40beb6f4ac30f8b5b
SALONI SINGH <ssaloni3258@gmail.com>
2026-05-04 23:32:00 +0530

    Update practice_question.py

--- practice_question.py
+++ practice_question.py
@@ -1,8 +1,18 @@
 #  — Recursion (Reverse a Number)
-def reverse_number(n, rev=0):
-    if n == 0:
-        return rev
-    return reverse_number(n // 10, rev * 10 + n % 10)
+# def reverse_number(n, rev=0):
+#     if n == 0:
+#         return rev
+#     return reverse_number(n // 10, rev * 10 + n % 10)
 
-print(reverse_number(1234))
-print(reverse_number(120))
+# print(reverse_number(1234))
+# print(reverse_number(120))
+
+# — Recursion (Logic-Based)
+def count_digits(n):
+    if n < 10:
+        return 1
+    return 1 + count_digits(n // 10)
+
+print(count_digits(12345))
+print(count_digits(7))
+print(count_digits(1000))
